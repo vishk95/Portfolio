3ddd27fceeace71d425f9b3ca008f41dd4ab2a16
Vishal Kardam <vishalat1994@gmail.com>
2019-08-12 23:43:41 +0530

    feat: Superslider and typed
    infused publicly available plugins like superslider and typed. Links for both will be added in the documentation.

--- js/script.js
+++ js/script.js
@@ -6,4 +6,12 @@ $(document).ready(function() {
 		pagination: false
 	});
 
+	var typed = new Typed(".typed", {
+		strings: ["Data Analyst.", "Web Developer.", "Student."],
+		typeSpeed: 70,
+		loop: true,
+		startDelay: 1000,
+		showCursor: false
+	});
+
 });
