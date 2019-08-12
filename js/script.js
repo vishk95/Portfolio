8e025e965693bf3aa6eeeb2834ea5a93cf33ae99
Vishal Kardam <vishalat1994@gmail.com>
2019-08-13 00:48:47 +0530

    feat: Added an about section with image

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
 	});
 
 	var typed = new Typed(".typed", {
-		strings: ["Data Analyst.", "Web Developer.", "Student."],
+		strings: ["Software Engineer.", "Web Developer.", "Student."],
 		typeSpeed: 70,
 		loop: true,
 		startDelay: 1000,
