b8219e9cc3424e0155f898f7493960a8179d7c73
Vishal Kardam <vishalat1994@gmail.com>
2019-08-14 04:05:52 +0530

    minor fixes

--- js/script.js
+++ js/script.js
@@ -16,7 +16,7 @@ $(document).ready(function() {
 	});
 
 	var typed = new Typed(".typed", {
-		strings: ["Software Engineer.", "Web Developer.", "Student."],
+		strings: ["Data Analyst.", "Web Developer.", "Student.", "Blogger."],
 		typeSpeed: 70,
 		loop: true,
 		startDelay: 1000,
